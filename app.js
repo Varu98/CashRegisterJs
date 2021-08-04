236148369e436fef12b3397b3ff80b9f918750d6
Vardaan <vardaan.agarwal.98@gmail.com>
2021-08-04 20:45:17 +0530

    add error handling feature

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const billAmt = document.querySelector("#bill-Amt");
-
 const cashAmt = document.querySelector("#cash-Amt");
 
-const btn = document.querySelector(".btn");
+const cashAmtDiv = document.querySelector(".cashAmt");
+const errorDiv = document.querySelector(".errorDiv");
 
+const btn = document.querySelector(".btn");
 const form = document.querySelector("#form");
 
-console.log(billAmt.value);
+let sum;
+
+form.addEventListener("submit", function onSubmit(e) {
+  e.preventDefault();
+  if (billAmt.value <= 0) {
+    showError("Enter A Valid Amount");
+    if (cashAmt.value < billAmt.value) {
+      showError(
+        `Ask The Customer To Pay ${billAmt.value - cashAmt.value} More`
+      );
+    }
+  } else {
+    sum = cashAmt.value - billAmt.value;
+    console.log(sum);
+  }
+});
+
+function showError(message) {
+  errorDiv.style.display = "block";
+  errorDiv.innerHTML = message;
+}
+
+function hideError() {
+  errorDiv.style.display = "none";
+}
